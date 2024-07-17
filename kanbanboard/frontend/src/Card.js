e9bd5979cc0a451090eed0ed4480c9c7b44ebdd7
parlyresk <parlyresk325@gmail.com>
2024-07-17 09:15:51 +0900

    클릭 화살표 처리

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './TaskList';
 import {_Card} from './assets/scss/Card.scss';
 
 function Card({ title, details, tasks }) {
+    const [isOpen,setIsOpen] = useState(false);
+    
+    
     return (
         <div className="_Card">
-            <div className="Card_Title">{title}</div>
+            <div className={`Card_Title ${isOpen ? 'Card_Title_Open' : ''}`}
+            onClick={()=>{setIsOpen(!isOpen)}}>{title}</div>
             <div className="Card_Details">
                 {details}
                 <TaskList tasks={tasks} />
