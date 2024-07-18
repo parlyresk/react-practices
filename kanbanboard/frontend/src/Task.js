bca591fded05d6f4f1ffaed1cbe4cb3b2cd3ea7b
parlyresk <parlyresk325@gmail.com>
2024-07-18 09:19:27 +0900

    done 체크 실행(data js에 적용x)

--- kanbanboard/frontend/src/Task.js
+++ kanbanboard/frontend/src/Task.js
@@ -1,18 +1,15 @@
 import React from 'react';
-
 import * as styles from './assets/scss/Task.scss';
 
-function Task({ text, done,setDone }) {
-    
-
-    return (
-        <li className={styles._Task}>
-            <input type="checkbox" checked={done}  
-            onClick={setDone} />
-            {text}
-            <a href="#" className={styles.Task_Remove}></a>
-        </li>
-    );
+function Task({ text, done, setDone }) {
+  return (
+    <li className={styles._Task}>
+      <input type="checkbox" checked={done}  
+             onChange={setDone} />
+      {text}
+      <a href="#" className={styles.Task_Remove}></a>
+    </li>
+  );
 }
 
 export default Task;
--- kanbanboard/frontend/src/TaskList.js
+++ kanbanboard/frontend/src/TaskList.js
@@ -1,24 +1,34 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Task from './Task';
-
+import update from 'react-addons-update';
 import * as styles from './assets/scss/TaskList.scss';
 
 function TaskList({ tasks }) {
-    const setDone = (index)=>{
-        index=!index;
-    }
+    
+    const [taskList, setTaskList] = useState([]);
+
+    useEffect(() => {
+        setTaskList(tasks);
+    }, [tasks]);
+
+    const toggleDone = (index) => {
+        const newTaskList = [...taskList];
+        newTaskList[index].done = !newTaskList[index].done;
+        setTaskList(newTaskList);
+    };
+    
     
     
       
     return (
         <div className={styles.Task_List}>
             <ul>
-                {tasks.map((task) => (
+                {taskList.map((task,index) => (
                     <Task 
                          
                         text={task.name} 
                         done={task.done} 
-                        setDone={setDone}
+                        setDone={() => toggleDone(index)}
                     />
                 ))}
             </ul>
