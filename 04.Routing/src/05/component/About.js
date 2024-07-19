239e65a4873c41d41af8b8a271c2f8a64bfdd4c3
parlyresk <parlyresk325@gmail.com>
2024-07-19 10:33:26 +0900

    routing 05

--- 04.Routing/src/05/component/About.js
+++ 04.Routing/src/05/component/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Header from '../layout/Header';
+import Navigation from '../layout/Navigation';
+import Footer from '../layout/Footer';
 import * as styles from '../assets/scss/component/About.scss';
 
 export default function About() {
     return (
+        <>
+        <Header />
             <div className={styles.About}>
-                <h2>kickscar 입니다.</h2>
+            <h2>kickscar 입니다.</h2>
             </div>
+            <Navigation/>
+            <Footer/>
+            </>
+            
     );
 }
